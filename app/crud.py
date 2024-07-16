40cd93a86592179541223e5e4e815349014bb5ca
IrinaTimoshina <106275489+IrinaTimoshina@users.noreply.github.com>
2024-07-16 12:27:14 +0300

    Add files via upload
    исправлен update_file

--- app/crud.py
+++ app/crud.py
@@ -1,43 +1,33 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
-from datetime import datetime
 
-
-def create_file(db: Session, file: schemas.FileCreate):
-    db_file = models.File(**file.dict())
+def create_file(db: Session, file: dict) -> models.File:
+    db_file = models.File(**file)
     db.add(db_file)
     db.commit()
     db.refresh(db_file)
     return db_file
 
-
-def get_files(db: Session):
+def get_files(db: Session) -> list[models.File]:
     return db.query(models.File).all()
 
+def get_file(db: Session, file_id: int) -> models.File:
+    return db.query(models.File).filter(models.File.id == file_id).first()
 
-def get_file_by_name(db: Session, file_name: str):
+def get_file_by_name(db: Session, file_name: str) -> models.File:
     return db.query(models.File).filter(models.File.name == file_name).first()
 
-
-def delete_file(db: Session, file: models.File):
-    db.delete(file)
-    db.commit()
-
-
-def update_file(db: Session, file_id: int, file_update: schemas.FileUpdate):
-    db_file = db.query(models.File).filter(models.File.id == file_id).first()
-    if not db_file:
-        return None
-
-    if file_update.name:
-        db_file.name = file_update.name
-    if file_update.path:
-        db_file.path = file_update.path
-    if file_update.comment:
-        db_file.comment = file_update.comment
-
-    db_file.updated_at = datetime.utcnow()
-
-    db.commit()
-    db.refresh(db_file)
+def update_file(db: Session, file_id: int, file_update: dict) -> models.File:
+    db_file = get_file(db, file_id)
+    if db_file:
+        for key, value in file_update.items():
+            setattr(db_file, key, value)
+        db.commit()
+        db.refresh(db_file)
     return db_file
+
+def delete_file(db: Session, file_id: int):
+    db_file = get_file(db, file_id)
+    if db_file:
+        db.delete(db_file)
+        db.commit()
